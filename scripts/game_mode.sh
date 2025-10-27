57420111bbbb35e642b613ab70726c0e018c1c58
KirTrub <kir.trubachev02@gmail.com>
2025-10-27 20:43:08 +0700

    1

--- scripts/game_mode.sh
+++ scripts/game_mode.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Проверяем, запущен ли swww-daemon
-if pgrep -x "swww-daemon" > /dev/null; then
+if pgrep -x "mpvpaper" > /dev/null; then
     echo "Останавливаю процессы..."
     pkill -x eww
-    pkill -x swww-daemon
+    pkill -x mpvpaper
     pkill -x waybar
 else
     echo "Запускаю окружение..."
     waypaper --restore &
-    eww open weather-window &
-    eww open time-window &
     ~/myDots/scripts/launch_waybar.sh
 fi
